e492058153f87dec2ea39765730712e5c7d6f0f3
Fellipe Tenorio <fellipe.tenorio@gmail.com>
2015-12-01 00:55:35 -0300

    Primeira versao da analise de Paredes.

--- ManagementRevitPlugin/ManagementRevitPlugin/Plugin.cs
+++ ManagementRevitPlugin/ManagementRevitPlugin/Plugin.cs
@@ -31,8 +31,24 @@ namespace ManagementRevitPlugin
                 ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
                 Document document = uidoc.Document;
 
-                FilterWalls(document);
-                
+                // File to hold text content
+                StringBuilder sb = new StringBuilder();
+                FilterWalls(document, sb);
+
+                using (StreamWriter outfile = new StreamWriter(@"C:\Revit\wall.txt", false))
+                {
+                    try
+                    {
+                        outfile.Write(sb.ToString());
+                        outfile.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        TaskDialog.Show("Revit","Não conseguiu criar o arquivo texto. Motivo: " + ex.Message);
+                        return Result.Failed;
+                    }
+
+                }
             }
             catch (Exception e)
             {
@@ -49,8 +65,10 @@ namespace ManagementRevitPlugin
 
         }
 
-        public static async void FilterWalls(Document document)
+        public static void FilterWalls(Document document, StringBuilder sb)
         {
+            sb.AppendLine("== Tratando Paredes ==");
+            StringBuilder temp = new StringBuilder();
             // Find all Wall instances in the document by using category filter
             ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
             // Apply the filter to the elements in the active document
@@ -58,36 +76,72 @@ namespace ManagementRevitPlugin
             FilteredElementCollector collector = new FilteredElementCollector(document);
             IList<Element> walls =
             collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+            sb.AppendLine(string.Format("Paredes encontradas: {0}", walls.Count));
             String prompt = string.Format("Total Walls: {0}. The walls in the current document are:\n", walls.Count);
             Wall wall;
-            Double total = 0d;
+            double total = 0d;
             Parameter parameter;
             int areaPerHour = 3;
-            double tempoTotal = 0;
+            double totalTime = 0;
             int index = 0;
+            double areaSquareMeters;
+
+            double timePerWall;
+
+            int bricksPerRootSquare = 30;
+            int bricksPerWall = 0;
+            int totalBricks = 0;
+
+            decimal cost100Bricks = 100;
+            decimal costBricksPerWall = 0;
+            decimal totalBricksCost = 0;
+
             foreach (Element e in walls)
             {
                 wall = e as Wall;
-                //StringBuilder sb = new StringBuilder();
-                //foreach (BuiltInParameter b in Enum.GetValues(typeof(BuiltInParameter)))
-                //{
-                //    //sb.AppendLine(string.Format("{0}: {1}", Enum.GetName(typeof(BuiltInParameter), b), wall.get_Parameter(b).AsValueString()));
-                //    //  Enum.GetName(typeof(BuiltInParameter), b), wall.get_Parameter(b)
+                
+                // Convert 
+                areaSquareMeters = UnitUtils.ConvertFromInternalUnits(wall.GetOrderedParameters()[20].AsDouble(), DisplayUnitType.DUT_SQUARE_METERS);
+                
 
-                //}
-                tempoTotal = wall.GetOrderedParameters()[21].AsDouble() / areaPerHour;
-                prompt += string.Format("Parede:{0}, Área: {1}. Tempo Necessário: {2} horas.\n", 
-                    ++index, 
-                    wall.GetOrderedParameters()[21].AsValueString(),
-                    tempoTotal);
-                //using (StreamWriter outfile = new StreamWriter(@"C:\Revit\wall.txt", true))
-                //{
-                //    await outfile.WriteAsync(sb.ToString());
-                //}
+                prompt += string.Format("Parede:{0}, Área: {1}. Tempo Necessário: {2} horas.\n", ++index, wall.GetOrderedParameters()[20].AsValueString(), totalTime);
 
+                // Timer
+                timePerWall = areaSquareMeters / areaPerHour;
+
+                // Material
+                // Total bricks equal to total area per number of bricker per wall square meters
+                bricksPerWall = (int) Math.Ceiling(areaSquareMeters * bricksPerRootSquare);
+
+                // Values
+                costBricksPerWall = bricksPerWall * cost100Bricks / 100;
+
+                // Update Totals
+                totalTime += timePerWall;
+                totalBricks += bricksPerWall;
+                totalBricksCost += costBricksPerWall;
+
+                // Log
+                temp.AppendLine(string.Format("Parede {0}: {1}", index, wall.Name));
+                temp.AppendLine(string.Format("Tempo de Construção: {0} horas", timePerWall));
+                temp.AppendLine(string.Format("Área: {0}", wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsValueString()));
+                temp.AppendLine(string.Format("Volume: {0}", wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsValueString()));
+                temp.AppendLine(string.Format("Comprimento: {0} m", wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsValueString()));
+                temp.AppendLine(string.Format("Altura: {0} m", wall.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsValueString()));
+                temp.AppendLine(string.Format("Tijolos Necessários: {0} unidades", bricksPerWall));
+                temp.AppendLine(string.Format("Custo do Tijolo: {0:C2}", costBricksPerWall));
+                // Calculates Materials
+
+                temp.AppendLine();
             }
-            prompt = "Tempo Total de Construção das paredes: " + tempoTotal+"\n"+prompt;
+            
+            prompt = "Tempo Total de Construção das paredes: " + totalTime+"\n"+prompt;
             TaskDialog.Show("Revit", prompt);
+            sb.AppendLine(string.Format("Tempo Total de Construção das Paredes: {0} horas", totalTime));
+            sb.AppendLine(string.Format("Total de tijolos necessários: {0} unidades", totalBricks));
+            sb.AppendLine(string.Format("Custo Total de tijolos: {0:C2}", totalBricksCost));
+            sb.AppendLine("--").AppendLine().Append(temp.ToString());
+            sb.AppendLine("== Fim Tratando Paredes ==");
         }
 
         public static void FilterDoors(Document document)
